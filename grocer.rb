afd62467cf2538c583e81eed8b94eac32a38946a
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-14 17:41:14 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,15 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  i = 0
+  while i < collection.length do
+    if name === collection[i][:item]
+      return collection[i]
+    else
+      return nil
+    end
+    i += 1
+  end
 end
 
 def consolidate_cart(cart)
@@ -9,18 +18,41 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  array_w_count = []
+  i = 0 
+  while i < cart.length do
+    if array_w_count[i].has_value?()
+    
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  i = 0 
+  while i < cart.length do
+    item_name = cart[i][:item]
+    if find_item_by_name_in_collection(item_name, coupon),
+      
+  #CONTENT
+  end
+   i += 1 
+ 
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  i = 0 
+  
+  while i < cart.length do 
+    if cart[i][:clearance]
+      cart[i][:price] = cart[i][:price] * 0.8
+    else
+      #do nothing if no clearance
+    end
+     i += 0 
 end
 
 def checkout(cart, coupons)
@@ -33,4 +65,21 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  apply_coupons(consolidated_cart, coupons)
+  apply_clearance(cart)
+  
+  consolidated_cart = consolidated_cart(cart)
+  coupons_applied_to_consolidated = apply_coupons(consolidated_cart, coupons)
+  clearance_and_coupons_applied = apply_clearance(coupons_applied_to_consolidated)
+  
+  total = 0.0
+  i = 0 
+  
+  while i < clearance_and_coupons_applied.length do
+    price_of_item = clearance_and_coupons_applied[i][:price]
+    float_price = price_of_item.to_f
+    total += float_price
+  end
+  i += 1
 end
+
